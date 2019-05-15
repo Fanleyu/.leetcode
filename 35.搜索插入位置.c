3abd53deaf1c6562208bc12741aa0bcf8f441e7e
Fanleyu <35924828+Fanleyu@users.noreply.github.com>
2019-05-15 16:00:39 +0800

    0515

--- 35.搜索插入位置.c
+++ 35.搜索插入位置.c
@@ -6,8 +6,30 @@
 
 
 int searchInsert(int* nums, int numsSize, int target){
-//
+    if (numsSize==0) return 0;
+    int right = numsSize-1;
+    int left = 0;
+    int mid = (left + right)/2;
+    while(left<=right)
+    {
+        if(nums[mid]<target)
+        {
+            left = mid+1;
+        }
+        else if(nums[mid]>target)
+        {
+            right = mid-1; 
+        }
+        else  
+            return mid;
+        mid = (left + right)/2;
+    }
+        if(left<=right)
+        {
+            return right;
+        }
+        else 
+            return left;
+    
 }
-
-
 
---
+++ 38.报数.c
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=38 lang=c
+ *
+ * [38] 报数
+ */
+
+
+char * countAndSay(int n){
+
+
+}
+
+
+
